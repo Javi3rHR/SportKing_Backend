870d7ef90d93ae8e668549a6e006153d9db1614d
Javier Herreros <javi3rhr@gmail.com>
2022-10-13 18:40:33 +0200

    First commit Entities User Role

---
+++ src/main/java/project/SportKingBackendApplication.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportKingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportKingBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/project/Users/entities/Role.java
@@ -0,0 +1,20 @@
+package project.Users.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+}
